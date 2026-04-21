dcfa390caea5e2c9d69bf80a3e1cdd1fc5990bbb
Ren Eric <eric_ren@connect-tech.sncf>
2026-04-21 10:36:53 +0200

    fix git fugitive

--- lua/plugins/vim-futigive.lua
+++ lua/plugins/vim-futigive.lua
@@ -35,9 +35,9 @@ return {
           if vim.bo.ft ~= 'fugitive' then return end
 
           local bufnr = vim.api.nvim_get_current_buf()
-          vim.keymap.set('n', '<leader>p', function() vim.cmd.Git 'push' end, { buffer = bufnr, remap = false, desc = '[G]it [P]ush' })
+          vim.keymap.set('n', '<leader>gP', function() vim.cmd.Git 'push' end, { buffer = bufnr, remap = false, desc = '[G]it [P]ush' })
 
-          vim.keymap.set('n', '<leader>P', function() vim.cmd.Git { 'pull', '--rebase' } end, { buffer = bufnr, remap = false, desc = '[G]it [P]ull' })
+          vim.keymap.set('n', '<leader>gp', function() vim.cmd.Git 'pull --rebase' end, { buffer = bufnr, remap = false, desc = '[G]it [P]ull' })
 
           vim.keymap.set('n', '<leader>t', ':Git push -u origin ', {
             buffer = bufnr,
